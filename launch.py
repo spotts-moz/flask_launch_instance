006fe0451e50e593087c3a29c532f31c7a69e262
spotts-moz <spotts61@gmail.com>
2016-06-30 03:01:22 -0700

    Updating args, setting up instance deconstruction
    Flags:
         corrected flag comments
         added flags for deconstruction, forcing changes and listing instances
    
    Functions:
         Added termination ability for 'running' and 'stopped' instances
         Added user prompt for both creation and termination of instances
         Added try/except for boto import to quickly inform users of the lack of the boto python module

--- launch.py
+++ launch.py
@@ -7,12 +7,17 @@
 #          (this script has been tested on Ubuntu 14.4, Windows 7/10, and OSX 10.11)
 #           The process is outlined here:   http://boto.cloudhackers.com/en/latest/boto_config_tut.html
 #
+#       - There is deconstruction available via the term flag allowing termination of instances when given the instance id desired.
+#             !!!Beware, termination means the loss/destruction of all local data on an instance!!!
+#                   (You can get a list of all running/stopped instances by using -term with any, even an invalid id and using '-list')
+#
 #   NOTES:
 #
 #       - This is a script written using Python 2.7 from the default install dir - Please note the initial shebang, 
 #           and the path variables may need to be adjusted to suit your installation/OS!!!
 #        
 #       - Requires the boto python module to be installed. It can be installed via "pip install boto"
+#               http://boto.cloudhackers.com/en/latest/getting_started.html
 #
 #       - If logging in via ssh is desired, you will need to use a public/private keypair from AWS. If you are not coming from a 10.0.0.0/8 add your public ip address below 
 #               ( If you need to find your external ip google "what is my IP" )
@@ -21,8 +26,8 @@
 #       - Output errors to log
 #       - Output to email, possibly a web hook
 #       - Retrieve AWS credentials from a secure store  ( may also be handled outside this script with configuration management such as puppet/hiera)
-#       - Re factor code out into more reasonable/smaller defs for readability, maintainability, and modularity  
-#       - Add security group rule evaluation and revoke. 
+#       - Re-factor code out into more reasonable/smaller defs for readability, maintainability, and modularity  
+#       - Add security group rule evaluation and revocation. 
 #       - Expand flexibility of security rule implementation 
 #       
 #
@@ -30,16 +35,20 @@
 ###################################################################
 
 
-
-
-import boto.ec2
 import time
 import os
 import sys
 import collections
 import argparse
 
-
+try:
+    import boto.ec2
+    
+except ImportError:
+    print "Error: The boto module is either not installed, failed to load, or has an improper path\nPlease see:\n    http://boto.cloudhackers.com/en/latest/getting_started.html"
+    sys.exit(2)
+    
+    
 #############################################################################################################
 # Configureation parameters                                                                                 # 
 #                                                                                                           # 
@@ -98,12 +107,38 @@ def readFileToVar(file):
         print "Error in readFileToVar"
         sys.exit(2)
 
-
+        
+def userPrompt(question, answers):
+    # This user prompt will be reusable for creating or terminating instances
+    # Setting up a switch to let us know when we have an acceptable user answer
+    acceptable = False
+    
+    while not acceptable:
+        print(question + "specify '%s' or '%s'") % answers
+        answer = raw_input()
+        if answer.lower() == answers[0].lower() or answers[0].lower():
+            print('You answered: %s') % answer
+            acceptable = True
+    return answer
 
 
 def createInstance():
     # TODO: have function accept reservation values as variables
     # Creating EC2 instance in us-east-1, where it is cheap
+    
+    if args.force:
+        pass
+        
+    elif not args.force:
+        user_answer = userPrompt("\nAre you sure you want to create this instance?! Instances can cost $!!!\n", ('y', 'n'))
+        if user_answer == 'y':
+            pass
+        else:
+            print "You must say yes to creating the instance, throw the '-f' flag to force, or not create an instance"
+            sys.exit(2)
+    
+    
+    
     try:
         conn = boto.ec2.connect_to_region(str(args.awsregion),
                                             aws_access_key_id = access_key_id,
@@ -143,14 +178,13 @@ def createInstance():
             sys.exit(2)
             
         try:
+            # Setting up our security group
             groups = [g for g in conn.get_all_security_groups() if g.name == my_security_group_name]
             group = groups[0] if groups else None
             if not group:
                 print "Creating group '%s'..."%(my_security_group_name,)
                 group = conn.create_security_group(my_security_group_name, "A group for %s"%(my_security_group_name,))
-                #current_rules = []
                 for rule in flask_test_rules:
-                    #current_rules.append(SecurityGroupRule(rule.ip_protocol, rule.from_port, rule.to_port, rule.grants[0].cidr_ip, None))
                     group.authorize(rule.ip_protocol,
                               rule.from_port,
                               rule.to_port,
@@ -208,11 +242,90 @@ def instanceCreationStatus(inst, update_method, attr_name, attr_value):
         except:
             print "Error while waiting, Retrying"
             time.sleep(15)
+            
 
-            
-            
+def terminateInstance(term_instance_id):
+    # Checking for, then terminating matching instances
+    
+    # Keeping track of whether or not we found a matching instance, and if prompted, the outcome
+    found_instance_id = False
+    delete_yes      = False
+    # If we're listing found instances, stick the ids here
+    instance_id_list = list()
+    
+    try:
+            # Setup AWS connection using our given region, access_key_id and secret_access_key
+        conn = boto.ec2.connect_to_region(str(args.awsregion),
+                                            aws_access_key_id = access_key_id,
+                                            aws_secret_access_key = secret_access_key)            
         
+        reservations = conn.get_all_reservations()
+        
+        # Comb through all our reservations in the given region
+        for r in reservations:
+            for i in r.instances:
+                # Did we ask for an instance list? If the instances aren't terminated store it in a list
+                if args.listinstances and not i.state == "terminated":
+                    instance_id_list.append(i.id)
+
+                # We are only acting on instances matching the given instance_id if it is 'running' or 'stopped'                    
+                # (Already terminated instances don't require our attention)
+                if (i.id == term_instance_id) and (i.state == "running" or i.state == "stopped"):
+                        
+                    # Setting our var so we get notice of its existence    
+                    found_instance_id = True
+                    
+                    # Are we forcing deletion? Terminate
+                    if args.force:
+                        conn.terminate_instances(instance_ids=[term_instance_id])
+                        # Setting our var so we know the outcome
+                        delete_yes = True
+                    
+                    # We aren't forcing deletes? Ask the user permission
+                    elif not args.force:
+                        prompt_answer = userPrompt("\nAre you sure you want to terminate this instance?! ALL DATA WILL BE LOST FROM THAT INSTANCE!!!\n", ('y', 'n'))
+
+                        # Our user confirmed termination? Terminate
+                        if prompt_answer == 'y':
+                            conn.terminate_instances(instance_ids=[term_instance_id])
+                            # Setting our var so we know the outcome
+                            delete_yes = True
+                            
+                        # Our user didn't confirm termination, move on    
+                        else:
+                            continue
+                
+                # If we found our instance but it's already terminated there's no need to act on it.
+                elif (i.id == term_instance_id) and i.state == "terminated":
+                    print "Instance %s is already terminated, nothing to act on" % term_instance_id
+                    sys.exit(0)
+                    
+                else:
+                    continue
+        
+        # If we asked for a list of the instances, print it here.
+        if len(instance_id_list) > 0:
+            print "\nList of instances found:\n%s" % instance_id_list
             
+    except boto.exception.BotoServerError:
+        print "Instance termination error - Check that regions, keys, and provided instance ids are correct."
+        sys.exit(2)     
+
+    # If our instance id exists and we deleted it tell us
+    if found_instance_id and delete_yes:
+        print "Matching instance was found and terminated"
+        sys.exit(0)
+    
+    # If we found our instance id but we didn't delete it tell us
+    elif found_instance_id and not delete_yes:
+        print "Matching instance found but not terminated"
+        sys.exit(0)
+        
+    # We didn't find our instance running or stopped, so let us know    
+    else:
+        print "No running or stopped instances with given id found"
+        sys.exit(2)
+    
 if __name__=="__main__":            
 
 
@@ -221,13 +334,25 @@ if __name__=="__main__":
         parser = argparse.ArgumentParser(description="This tool reads json data to calculate percentile points.")
 
         #   Setting up valid CLI args
-        #  Breaking out the user data opens up this launch script to configure launched instances any way you need.
+        # The force flag can be used to bypass any user prompts - !!!Beware ultimate power!!!
+        parser.add_argument("-f", "--force",
+                        help="Forcing the command to run without user prompts",
+                        action="store_true", default=False)
+                        
+        # In case a user doesn't have access to the AWS console and lost the Instance ID for an instance they
+        #    want to terminate, they can use this in concert with '-term'. (A *valid* instance id is not necessary to see the list!)
+        parser.add_argument("-list", "--listinstances",
+                        help="Used in conjunction with '-term' to allow users to see what instance ids are available. (A valid instance id is not necessary for this)",
+                        action="store_true", default=False)                        
+                        
+                        
+        #  Breaking out the user data opens up this launch script to configure launched instances in multiple configurations.
         parser.add_argument("-ud", "--userdata",
                         help="Specifies a user data file instead of the default",
                         action="store", default=False)
 
                         
-        #  Breaking out the key store dir allows us to place new keys in a shared store.
+        #  Breaking out the key store dir allows us to place new keys in a shared store, etc.
         parser.add_argument("-kd", "--keydir",
                         help="Specifies a key directory instead of the default",
                         action="store", default=False)
@@ -242,21 +367,26 @@ if __name__=="__main__":
                         help="Specifies a key pair name beyond the default",
                         action="store", default=False)
                         
-        #  Breaking out the key pair name to open options
+        #  Breaking out aws region to open options
         parser.add_argument("-ar", "--awsregion",
                         help="Specifies the AWS region you want to create an instance in. Default: 'us-east-1'  Ensure your AMI is available for that region!",
                         action="store", default='us-east-1')
 
-        #  Breaking out the key pair name to open options
+        #  Breaking out the ami name to open options (To ensure compatability another puppet config may be necessary)
         parser.add_argument("-am", "--ami",
                         help="Specifies AMI you want to use create an instance. Default: 'ami-fce3c696'  Ensure your AMI is available for that region!",
                         action="store", default='ami-fce3c696')                     
                         
-        #  Breaking out the key pair name to open options
+        #  Breaking out the instance type we'll create, to open options
         parser.add_argument("-it", "--instancetype",
                         help="Specifies the instance type you want to spin up. Default: 't2.micro'  Ensure your instance type is available for that region!",
                         action="store", default='t2.micro')                         
                         
+        #  Setting up deconstruction. This is also available in the AWS console. (Please see AWS documentation)
+        parser.add_argument("-term", "--terminate",
+                        help="When terminating, not creating an instance specify instance id, ie; 'i-00000000'",
+                        action="store")
+
                         
         # Parse arguments
         args = parser.parse_args()                        
@@ -306,11 +436,15 @@ if __name__=="__main__":
                 if args.keypair:
                     my_keypair_name = str(args.keypair)
                 
-                new_instance = createInstance()
+                if not args.terminate:
+                    new_instance = createInstance()
             
-                # Use instance id and watch its status.         
-                instanceCreationStatus(new_instance.instances[0], "update", "state", "running")               
+                    # Use instance id and watch its status.         
+                    instanceCreationStatus(new_instance.instances[0], "update", "state", "running")
 
+                else:
+                    # If args.terminate is populated, we look to terminate the given instance
+                    terminateInstance(args.terminate)
                     
                     
             except Exception as e:
